da0663fc2a4c74d2425fe4b5cbe08040c571418a
notbabaisyou <though-went-some-simple@proton.me>
2025-07-17 10:56:43 +0200

    fission: Add license notices.
    Also renames the screen name so that we can tell the difference between modesetting.

--- hw/xfree86/drivers/fission/driver.c
+++ hw/xfree86/drivers/fission/driver.c
@@ -1,3 +1,20 @@
+/**
+ * Spaghetti Display Server
+ * Copyright (C) 2025  SpaghettiFork
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /*
  * Copyright 2008 Tungsten Graphics, Inc., Cedar Park, Texas.
  * Copyright 2011 Dave Airlie
@@ -155,7 +172,7 @@ static MODULESETUPPROTO(Setup);
 
 static XF86ModuleVersionInfo VersRec = {
     .modname      = "fission",
-    .vendor       = MODULEVENDORSTRING,
+    .vendor       = "Spaghetti Fork",
     ._modinfo1_   = MODINFOSTRING1,
     ._modinfo2_   = MODINFOSTRING2,
     .xf86version  = XORG_VERSION_CURRENT,
@@ -365,7 +382,7 @@ ms_setup_scrn_hooks(ScrnInfoPtr scrn)
 {
     scrn->driverVersion = 1;
     scrn->driverName = "fission";
-    scrn->name = "modeset";
+    scrn->name = "fission";
 
     scrn->Probe = NULL;
     scrn->PreInit = PreInit;
