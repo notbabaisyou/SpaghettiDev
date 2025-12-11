aaddee8459f551694516af3f6ef41a7037e563d8
notbabaisyou <though-went-some-simple@proton.me>
2025-12-11 16:25:26 +0100

    glamor: Invalidate the current FBO on flush
    This _should_ be faster than flushing or finishing the entire render pipeline.
    
    Also revert the dirty state tracking since we're no longer providing XWayland.

--- glamor/glamor_utils.h
+++ glamor/glamor_utils.h
@@ -672,16 +672,17 @@ glamor_make_current(glamor_screen_private *glamor_priv)
         lastGLContext = glamor_priv->ctx.ctx;
         glamor_priv->ctx.make_current(&glamor_priv->ctx);
     }
-    glamor_priv->dirty = TRUE;
 }
 
 static inline void
 glamor_flush(glamor_screen_private *glamor_priv)
 {
-    if (glamor_priv->dirty) {
-        glamor_make_current(glamor_priv);
-        glFlush();
-        glamor_priv->dirty = FALSE;
+    GLint fbo;
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &fbo);
+
+    if (fbo) {
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
     }
 }
 
