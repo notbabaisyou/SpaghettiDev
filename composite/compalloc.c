notbabaisyou/SpaghettiDev

commit b933aa414422326a69671991623af805fa52f10a

composite: Skip copying parent pixmap contents when possible
If the parent window has a different depth (which means pWin can't have
valid contents yet) and pWin has effective background other than None.

composite/compalloc.c
@@ -526,6 +526,15 @@ compUnredirectOneSubwindow(WindowPtr pParent, WindowPtr pWin)
     return Success;
 }
 
+static unsigned
+compGetBackgroundState(WindowPtr pWin)
+{
+    while (pWin->backgroundState == ParentRelative)
+        pWin = pWin->parent;
+
+    return pWin->backgroundState;
+}
+
 static PixmapPtr
 compNewPixmap(WindowPtr pWin, int x, int y, int w, int h)
 {
@@ -542,7 +551,8 @@ compNewPixmap(WindowPtr pWin, int x, int y, int w, int h)
     pPixmap->screen_x = x;
     pPixmap->screen_y = y;
 
-    if (pParent->drawable.depth == pWin->drawable.depth) {
+    if (pParent->drawable.depth == pWin->drawable.depth)
+    {
         GCPtr pGC = GetScratchGC(pWin->drawable.depth, pScreen);
 
         if (pGC) {
@@ -560,7 +570,8 @@ compNewPixmap(WindowPtr pWin, int x, int y, int w, int h)
             FreeScratchGC(pGC);
         }
     }
-    else {
+    else if (compGetBackgroundState(pWin) == None)
+    {
         PictFormatPtr pSrcFormat = PictureWindowFormat(pParent);
         PictFormatPtr pDstFormat = PictureWindowFormat(pWin);
         XID inferiors = IncludeInferiors;
@@ -592,6 +603,7 @@ compNewPixmap(WindowPtr pWin, int x, int y, int w, int h)
         if (pDstPicture)
             FreePicture(pDstPicture, 0);
     }
+
     return pPixmap;
 }
 

commit 51e944316954ef7cb5980ddc288663429e7e878c

composite: Inline compGetBackgroundState
Fixes: b933aa414422326a69671991623af805fa52f10a

composite/compalloc.c
@@ -526,7 +526,7 @@ compUnredirectOneSubwindow(WindowPtr pParent, WindowPtr pWin)
     return Success;
 }
 
-static unsigned
+static inline unsigned
 compGetBackgroundState(WindowPtr pWin)
 {
     while (pWin->backgroundState == ParentRelative)