23743ed764bf9e60fbf4cf0479f29e4d25f4e04b
notbabaisyou <though-went-some-simple@proton.me>
2026-02-10 21:23:47 +0100

    dix/events: Add missing NULL check in GetClientsForDelivery
    Also tidies up the function.

--- dix/events.c
+++ dix/events.c
@@ -2224,29 +2224,35 @@ static Bool
 GetClientsForDelivery(DeviceIntPtr dev, WindowPtr win,
                       xEvent *events, Mask filter, InputClients ** iclients)
 {
-    int rc = 0;
+    Bool rc = FALSE;
 
-    if (core_get_type(events) != 0)
+    if (core_get_type(events) != 0) {
         *iclients = (InputClients *) wOtherClients(win);
-    else if (xi2_get_type(events) != 0) {
+    } else if (xi2_get_type(events) != 0) {
         OtherInputMasks *inputMasks = wOtherInputMasks(win);
+
+        if (!inputMasks)
+            goto out;        
 
         /* Has any client selected for the event? */
         if (!WindowXI2MaskIsset(dev, win, events))
             goto out;
+
         *iclients = inputMasks->inputClients;
-    }
-    else {
+    } else {
         OtherInputMasks *inputMasks = wOtherInputMasks(win);
 
+        if (!inputMasks)
+            goto out;
+
         /* Has any client selected for the event? */
-        if (!inputMasks || !(inputMasks->inputEvents[dev->id] & filter))
+        if (!(inputMasks->inputEvents[dev->id] & filter))
             goto out;
 
         *iclients = inputMasks->inputClients;
     }
 
-    rc = 1;
+    rc = TRUE;
  out:
     return rc;
 }
