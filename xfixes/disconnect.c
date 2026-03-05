e08621664b29a374de0380dc4409c5a3dcf3eb9b
Demi Marie Obenour <demiobenour@gmail.com>
2026-03-05 08:55:10 +0100

    Add AllowForceTerminate to xorg.conf
    Forced termination is only allowed on bare hardware, not when running in
    another window system.

--- xfixes/disconnect.c
+++ xfixes/disconnect.c
@@ -62,6 +62,8 @@ typedef struct _ClientDisconnect {
     ((ClientDisconnectPtr) dixLookupPrivate(&(s)->devPrivates, \
                                             ClientDisconnectPrivateKey))
 
+Bool allowForceTerminate;
+
 int
 ProcXFixesSetClientDisconnectMode(ClientPtr client)
 {
@@ -80,6 +82,8 @@ ProcXFixesSetClientDisconnectMode(ClientPtr client)
      * and have permission to manage the server.
      */
     if (stuff->disconnect_mode & XFixesClientDisconnectFlagForceTerminate) {
+        if (!allowForceTerminate)
+            return BadAccess;
         if (!ClientIsLocal(client))
             return BadAccess;
         if ((rc = XaceHook(XACE_SERVER_ACCESS, client, DixManageAccess)))
