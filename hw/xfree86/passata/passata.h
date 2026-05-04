93b9a4b727b3c15db08ea90ee81071c30d5315ee
notbabaisyou <though-went-some-simple@proton.me>
2026-05-04 22:23:52 +0200

    passata: Initial stub.

---
+++ hw/xfree86/passata/passata.h
@@ -0,0 +1,34 @@
+/**
+ * Spaghetti Display Server
+ * Copyright (C) 2026  SpaghettiFork
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PASSATA_H
+#define PASSATA_H
+
+#include <xf86str.h>
+
+#include "scrnintstr.h"
+
+#define PASSATA_EGL_MODULE_NAME "passata"
+
+_X_EXPORT Bool
+passata_init(ScrnInfoPtr scrn, int fd);
+
+_X_EXPORT void
+passata_fini(ScrnInfoPtr scrn);
+
+#endif /* PASSATA_H */
