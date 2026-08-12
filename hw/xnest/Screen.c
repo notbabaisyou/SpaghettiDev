ad96c9ed442130e85a6d8d6acd592477191ad2e9
notbabaisyou <though-went-some-simple@proton.me>
2026-08-12 18:32:59 +0200

    xnest: stop clobbering pScreen->devPrivate during screen setup
    Remove the stale devPrivate = NULL in xnestOpenScreen that clobbered the
    miScreenInitParmsRec left by miScreenInit()/miScreenDevPrivateInit(),
    which made miCreateScreenResources() fail with "failed to create screen
    resources". Use the new miScreenClose() for teardown.

--- hw/xnest/Screen.c
+++ hw/xnest/Screen.c
@@ -20,6 +20,8 @@ is" without express or implied warranty.
 #include <X11/Xdefs.h>
 #include <X11/Xproto.h>
 
+#include "mi/mi_priv.h"
+
 #include "scrnintstr.h"
 #include "dix.h"
 #include "mi.h"
@@ -261,7 +263,6 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
     pScreen->blackPixel = xnestBlackPixel;
     /* GCperDepth */
     /* defaultStipple */
-    pScreen->devPrivate = NULL;
     /* WindowPrivateLen */
     /* WindowPrivateSizes */
     /* totalWindowSize */
@@ -423,7 +424,7 @@ xnestCloseScreen(ScreenPtr pScreen)
         free(pScreen->allowedDepths[i].vids);
     free(pScreen->allowedDepths);
     free(pScreen->visuals);
-    free(pScreen->devPrivate);
+    miScreenClose(pScreen);
 
     /*
        If xnestDoFullGeneration all x resources will be destroyed upon closing
