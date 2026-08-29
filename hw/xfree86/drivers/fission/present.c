432e2f4dcfabd63cce129f2fb9a530daa3986502
notbabaisyou <though-went-some-simple@proton.me>
2026-08-29 15:32:03 +0200

    fission: Signal SuboptimalCopy to the application between sync/async flips
    Fixes horrible flickering on native Vulkan applications.

--- hw/xfree86/drivers/fission/present.c
+++ hw/xfree86/drivers/fission/present.c
@@ -343,7 +343,6 @@ ms_present_check_flip(RRCrtcPtr crtc,
 
     if (reason && async_flip != ms_window_has_async_flip_modifiers(window)) {
         *reason = PRESENT_FLIP_REASON_BUFFER_FORMAT;
-        goto no_flip;
     }
 
     ms->flip_window = window;
